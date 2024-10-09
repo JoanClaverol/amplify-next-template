800f88fdddd717c8f2a816885506e60ff8edb45b
Your Name <you@example.com>
2024-10-09 19:58:57 +0200

    changed teh apiconfig file wiht the right lik to urls

--- app/constants/apiConfig.ts
+++ app/constants/apiConfig.ts
@@ -1,3 +1,3 @@
 // app/constants/apiConfig.ts
-export const API_BASE_URL = "http://127.0.0.1:3006/";
-//   "https://y3fglnw1n3.execute-api.eu-west-3.amazonaws.com/Prod";
+export const API_BASE_URL =
+  "https://y3fglnw1n3.execute-api.eu-west-3.amazonaws.com/Prod";
