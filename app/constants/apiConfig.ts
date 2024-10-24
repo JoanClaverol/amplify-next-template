f17caf114f7486100fbe940c1b1f13ec83972972
Your Name <you@example.com>
2024-10-24 21:43:53 +0200

    updated backend link

--- app/constants/apiConfig.ts
+++ app/constants/apiConfig.ts
@@ -1,3 +1,2 @@
-export const API_URL = "http://127.0.0.1:3000/";
-
-//   "https://y3fglnw1n3.execute-api.eu-west-3.amazonaws.com/Prod/";
+export const API_URL =
+  "https://y3fglnw1n3.execute-api.eu-west-3.amazonaws.com/Prod/";
